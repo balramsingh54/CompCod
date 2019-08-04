d16c8d73351fa172f9d01621b8c3b7051900000a
Balram <singhbalram054@gmail.com>
2019-08-04 22:16:44 +0530

    built in function

--- c++QuesAns/function/function_built_in.cpp
+++ c++QuesAns/function/function_built_in.cpp
@@ -2,13 +2,31 @@
 #include <cmath>
 using namespace std;
 
+void userdefine()
+{
+	cout<<"this is userdefine function"<<endl;
+}
+
+int user()
+{
+	return 6;
+}
+
 int main(int argc, char const *argv[])
 {
 	cout<<cbrt(27)<<endl;
 	cout<<ceil(27.8)<<endl;
 	cout<<abs(-27.3)<<endl;
 	cout<<cos(sqrt(2))<<endl;
+	userdefine();
+	cout<<user();
 
 
 	return 0;
+
 }
+
+// void userdefine()  This will give error becouse userdefined function 
+// {
+// 	cout<<"this is userdefine function"<<endl;
+// }
