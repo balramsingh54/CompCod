4d325f429e49a390fd26dec44709c74754239386
Balram <singhbalram054@gmail.com>
2019-08-04 21:11:15 +0530

    function

---
+++ c++QuesAns/function/function_built_in.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout<<cbrt(27)<<endl;
+	cout<<ceil(27.8)<<endl;
+	cout<<abs(-27.3)<<endl;
+	cout<<cos(sqrt(2))<<endl;
+
+
+	return 0;
+}
