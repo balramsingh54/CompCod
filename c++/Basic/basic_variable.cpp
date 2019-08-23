4b07df39d730b249cc476be103b3d789dd706c8e
Balram <singhbalram054@gmail.com>
2019-08-23 14:21:31 +0530

    basic

--- c++/Basic/basic_variable.cpp
+++ c++/Basic/basic_variable.cpp
@@ -1,18 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int main(int argc, char const *argv[])
+// {
+// 	auto int x=5;
+// 	{
+// 		auto int x=6;
+// 		{
+// 			auto int x=7;
+// 			cout<<x<<endl;
+// 		}
+// 		cout<<x<<endl;
+// 	}
+// 	cout<<x;
+
+// 	return 0;
+// }
+
 int main(int argc, char const *argv[])
 {
 	auto int x=5;
 	{
-		auto int x=6;
+		cout<<x<<endl;
 		{
-			auto int x=7;
+			cout<<x<<endl;
+			auto int x=6;
 			cout<<x<<endl;
 		}
 		cout<<x<<endl;
 	}
-	cout<<x;
+
 
 	return 0;
 }
+
