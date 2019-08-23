7a823381a99fd56ab1e86ec68ebe711b2c2e2c3c
Balram <singhbalram054@gmail.com>
2019-08-23 15:23:23 +0530

    switch

---
+++ c++/switch/switch.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int s=3;
+	switch(s)
+	{
+		case 1:
+		cout<<"this is case 1 ";
+		break;
+		case 2:
+		cout<<"this is case 2";
+		break;
+		case 3:
+		cout<<"this is case 3";
+		break;
+		default:
+		cout<<"invalid input";
+
+	}
+
+	return 0;
+}
