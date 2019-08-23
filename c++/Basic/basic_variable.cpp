df52cebc0a2ffb3c785d579b56504606d05ee025
Balram <singhbalram054@gmail.com>
2019-08-23 14:45:34 +0530

    basic

--- c++/Basic/basic_variable.cpp
+++ c++/Basic/basic_variable.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// automatic variable 
+
 // int main(int argc, char const *argv[])
 // {
 // 	auto int x=5;
@@ -17,20 +19,55 @@ using namespace std;
 // 	return 0;
 // }
 
+// int main(int argc, char const *argv[])
+// {
+// 	auto int x=5;
+// 	{
+// 		cout<<x<<endl;
+// 		{
+// 			cout<<x<<endl;
+// 			auto int x=6;
+// 			cout<<x<<endl;
+// 		}
+// 		cout<<x<<endl;
+// 	}
+
+
+// 	return 0;
+// }
+
+/// output is 1 2 3 
+
+// void demo_static()
+// {
+// 	static int x=0;
+// 	cout<<x<<endl;
+// 	x++;
+// }
+
+// int main(int argc, char const *argv[])
+// {
+// 	demo_static();
+// 	demo_static();
+// 	demo_static();
+// 	return 0;
+// }
+
+void demo_static()
+{
+	int x=0;
+	cout<<x<<endl;
+	x++;
+}
+
 int main(int argc, char const *argv[])
 {
-	auto int x=5;
-	{
-		cout<<x<<endl;
-		{
-			cout<<x<<endl;
-			auto int x=6;
-			cout<<x<<endl;
-		}
-		cout<<x<<endl;
-	}
-
-
+	demo_static();
+	demo_static();
+	demo_static();
 	return 0;
 }
+
+/// output is 0 0 0 
+
 
