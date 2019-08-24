e1929cb53d3b4a67507c13618b421c4e411b25aa
Balram <singhbalram054@gmail.com>
2019-08-24 12:24:22 +0530

    functioon

--- c++/Basic/inc_dec.cpp
+++ c++/Basic/inc_dec.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
-
-int main(int argc, char const *argv[])
+int main()
 {
-	
-	return 0;
+   int var=5;
+   // 1st it will increased value by 1 and then print it 6
+   cout << ++var << endl;
+   // 5 is displayed then, var is increased to 6.
+   cout << var++ << endl;
+   // Initially, var = 6. It is increased to 7 then, it is displayed.
+   cout << ++var << endl;
+   return 0; 
 }
+
+//   output is 6 6 8
---
+++ c++/function/prime_no_bt_no.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int prime_no(int a, int b)
+{
+	
+}
+
+
+int main(int argc, char const *argv[])
+{
+	int a, b;
+	cin>>a>>b;
+
+
+	return 0;
+}
