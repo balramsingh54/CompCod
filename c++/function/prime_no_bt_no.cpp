70cf90f102415ae3c0fb24e34606a13b18d455b8
Balram <singhbalram054@gmail.com>
2019-08-25 11:29:06 +0530

    prime nu bt two number

---
+++ c++/function/prime_no_bt_no.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int CheckPrimeNo(int n)
+{
+	bool flag;
+	for (int i = 2; i < n; i++)
+	{
+		if (n%i==0)
+		{
+			flag=1;
+			break;
+		}
+		if (flag==0)
+		{
+			return n;
+		}
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int n1, n2;
+	bool flag;
+	cin>>n1>>n2;
+	for (int i = n1; i <= n2; i++)
+	{
+		flag=CheckPrimeNo(i);
+		if (flag)
+		{
+			cout<<i<<", ";
+		}
+	}
+
+
+
+	return 0;
+}
