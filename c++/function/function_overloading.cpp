72eb812c5ef5a92df4d2d4acdded04acdf39932b
Balram <singhbalram054@gmail.com>
2019-08-25 19:43:28 +0530

    overloading

---
+++ c++/function/function_overloading.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void display(int a)
+{
+	cout<<a<<endl;
+}
+int display(float b)
+{
+	return b;
+}
+void display(char c)
+{
+	cout<<c<<endl;
+}
+
+int main(int argc, char const *argv[])
+{
+	int a=3;
+	int b=5.5;
+	char d='c';
+	display(a);
+	display(b);
+	display(d);
+
+	return 0;
+}
