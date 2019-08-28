d06fcf847aef59ecd353665a47cf4e896478c27c
Balram <singhbalram054@gmail.com>
2019-08-28 15:28:37 +0530

    string

--- c++/string/basicstring.cpp
+++ c++/string/basicstring.cpp
@@ -4,14 +4,35 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	//string str;
-	string st;
-	//char str[30];
-	// cin>>str;
-	//cin.get(str, 30);
-	getline(cin, st);
-	//cout<<str;
-	cout<<st;
+	// //string str;
+	// string st;
+	// //char str[30];
+	// // cin>>str;
+	// //cin.get(str, 30);
+	// getline(cin, st);
+	// //cout<<str;
+	// cout<<st;
+
+
+	// char ch='A';
+	// char* ptr=&ch;
+	// cout<<ch;
+	// cout<<ptr;
+
+	char arr[]="Balram";
+	int brr[]={3,5,6,3,4};
+	// cout<<arr<<endl;
+	// cout<<brr;
+
+	// char arr[] = {'a','b','c','\0'};
+	// char brr[] = "abc";
+
+	cout<<arr<<endl;
+	cout<<sizeof(arr)<<endl;
+
+	cout<<brr<<endl;
+	cout<<sizeof(brr)<<endl;
 
 	return 0;
 }
+
