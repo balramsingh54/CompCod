a696ee2e241299e10fc21b535018cb25547854f2
Balram <singhbalram054@gmail.com>
2019-08-28 14:52:29 +0530

    string

---
+++ c++/string/basicstring.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	//string str;
+	string st;
+	//char str[30];
+	// cin>>str;
+	//cin.get(str, 30);
+	getline(cin, st);
+	//cout<<str;
+	cout<<st;
+
+	return 0;
+}
