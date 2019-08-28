faf875c0ef56bf1b2256098c5fa1854b65137096
Balram <singhbalram054@gmail.com>
2019-08-28 16:04:46 +0530

    palindrome

---
+++ c++/string/reverse_string.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void reverse()
+{
+	string str="Balram";
+	int left=0;
+	int size= str.length();
+	int right=size-1;
+	while(left<right)
+	{
+		char ch=str[left];
+		str[left]=str[right];
+		str[right]=ch;
+		left++;
+		right--;
+	}
+	cout<<str;
+
+}
+
+bool isPalindrome()
+{
+	string str="na";
+	int left=0;
+	int right= str.length()-1;
+	if (str[left]!=str[right])
+	{
+		return false;
+	}
+	return true;
+}
+
+int main(int argc, char const *argv[])
+{
+
+	reverse();
+	cout<<endl;
+	cout<<isPalindrome();
+	return 0;
+}
