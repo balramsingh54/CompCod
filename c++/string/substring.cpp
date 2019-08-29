29360adfe6dac9669626f04771ed55f5d819e8c0
Balram <singhbalram054@gmail.com>
2019-08-30 05:21:40 +0530

    string

--- c++/string/substring.cpp
+++ c++/string/substring.cpp
@@ -3,7 +3,14 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	string str="Hello";
-	cout<<str.substr(1);
+	cout<<str.substr()<<endl;
+	for (int i = 0; i < str.length(); i++)
+	{
+		for (int j = i; j < str.length()+1; j++)
+		{
+			cout<<str.substr(i, j)<<endl;
+		}
+	}
 
 
 	return 0;
