411fb76f75825dfdc5442776ff2ff98fb2a47c82
Balram <singhbalram054@gmail.com>
2019-08-31 00:54:38 +0530

    vowel

---
+++ c++/string/delete.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void deletee(string str)
+{
+	
+}
+
+
+int main(int argc, char const *argv[])
+{
+
+	return 0;
+}
---
+++ c++/string/remove_all.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+string remove_Char()
+{
+	string str= "thief is called as 420. This is code number";
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (0 <= str[i]  and str[i] >=9)
+		{
+			
+		}
+	}
+
+}
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
