2baac740a35f89c3ae900813e3f58890909b3d32
Balram <singhbalram054@gmail.com>
2019-08-31 16:46:09 +0530

    alphabet3

---
+++ c++/pattern/alphbet3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int size;
+	cin>>size;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < i+1; j++)
+		{
+			cout<<char(65+j);
+		}
+		cout<<endl;
+	}
+
+
+	return 0;
+}
