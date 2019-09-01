546ebb57bf98e456d52d4502e97351d3c964b94e
Balram <singhbalram054@gmail.com>
2019-09-01 11:05:13 +0530

    class and object

--- c++/oops/class_object.cpp
+++ c++/oops/class_object.cpp
@@ -1,11 +1,25 @@
+// object and class method 
 #include <iostream>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
 	class student
+	{
+	public:
+		int id;
+		string name;
+		float salery;
+	};
 
+	student s1;
+	s1.id=1;
+	s1.name="Balram";
+	s1.salery= 30000;
 
+	cout<<s1.id<<endl;
+	cout<<s1.name<<endl;
+	cout<<s1.salery;
 
 	return 0;
 }
