0317891e9c62b6c1534d91e688ba24bf81df3051
Balram <singhbalram054@gmail.com>
2019-09-07 12:53:17 +0530

    stl stack

--- DataStru/stack/stack_stl.cpp
+++ DataStru/stack/stack_stl.cpp
@@ -6,6 +6,15 @@ int main(int argc, char const *argv[])
 {
 	stack <int> st;
 
-	cout<<st.empty();
+	cout<<st.empty()<<endl;
+	st.push(500);
+	//cout<<"top element is : "<<st.top()<<endl;
+	st.push(400);
+	st.push(300);
+	st.push(200);
+	st.push(100);
+	cout<<"top element is : "<<st.top()<<endl;
+	cout<<st.size()<<endl;
+
 	return 0;
 }
