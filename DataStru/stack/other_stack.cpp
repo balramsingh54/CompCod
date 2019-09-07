3110445b9e00c32b3ee141aa962dc4fea7347210
Balram <singhbalram054@gmail.com>
2019-09-07 20:37:52 +0530

    transfer

--- DataStru/stack/other_stack.cpp
+++ DataStru/stack/other_stack.cpp
@@ -1,1 +1,42 @@
 #include <iostream>
+#include <stack>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	stack <int> st1;
+	stack<int> st2;
+	cout<<endl;
+	cout<<"****...stack 1 is going on ...****"<<endl;
+	st1.push(100);
+	cout<<st1.top()<<endl;
+	st1.push(200);
+	cout<<st1.top()<<endl;
+	st1.push(300);
+	cout<<st1.top()<<endl;
+	st1.push(400);
+	cout<<st1.top()<<endl;
+	st1.push(500);
+	cout<<st1.top()<<endl;
+
+	cout<<"---------------------------"<<endl;
+
+	for (int i = 1; i < st1.size()+2; ++i)
+	{
+		int x=st1.top();
+		st2.push(x);
+		st1.pop();
+		cout<<st2.top()<<endl;
+
+	}
+
+	return 0;
+}
+
+
+/*  500
+	400
+	300
+	200
+	100
+*/
