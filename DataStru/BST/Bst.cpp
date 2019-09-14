414e9401c56d77bda8bac538d4a4ae335295fdc7
Balram <singhbalram054@gmail.com>
2019-09-14 15:52:23 +0530

    BST

---
+++ DataStru/BST/Bst.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+	int data;
+	node* left;
+	node* right;
+
+	node(int d)
+	{
+		this->data=d;
+		left=NULL;
+		right=NULL;
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	
+
+	return 0;
+}
