51d54348ee02e74cfa3cf55be7a67f632d345d79
Balram <singhbalram054@gmail.com>
2019-09-19 22:43:35 +0530

    all

---
+++ c++/string/clear_doubt.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	char ch='A';
+	n=ch;
+	cout<<n;
+
+	return 0;
+}
---
+++ c++/string/reverse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int arr[5]= {1,2,3,4,5};
+	int s=0;
+	int e=4;
+	int temp;
+	int brr[5]={0};
+	while(s<=e)
+	{
+		
+	}
+
+
+	return 0;
+}
