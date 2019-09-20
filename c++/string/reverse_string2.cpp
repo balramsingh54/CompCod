fef8cee1ee88d6cdd328a149fb02bfbb1f2e428c
Balram <singhbalram054@gmail.com>
2019-09-20 06:06:36 +0530

    reverse string2

---
+++ c++/string/reverse_string2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void reverse_string(string str)
+{
+	int temp;
+	int s=0;
+	int e=str.length()-1;
+	while(s<=e)
+	{
+		temp=str[s];
+		str[s]=str[e];
+		str[e]=temp;
+		s++;
+		e--;
+	}
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		cout<<str[i]<<" ";
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	string str ="Balram";
+
+	reverse_string(str);
+	return 0;
+}
