3e4ce6e4fbc0eaa32671a42cf3704817248e04f0
Balram <singhbalram054@gmail.com>
2019-09-20 16:41:38 +0530

    palindrome_Done

--- c++/string/palondrome.cpp
+++ c++/string/palondrome.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 using namespace std;
 
-bool ispalindrome(string str)
+void ispalindrome(string str)
 {
-	
+	bool flag=0;
+	int s=0;
+	int e=str.length()-1;
+	while(s<=e)
+	{
+		if (str[s]!=str[e])
+		{
+			//cout<<"This is not palindrome:";
+			flag=1;
+			break;
+		}
+		s++;
+		e--;
+	}
+	if (flag==1)
+	{
+		cout<<"this is not palindrome:";
+	}
+	else
+	{
+		cout<<"this is palindrome:";
+	}
+
 }
 
 int main(int argc, char const *argv[])
 {
-	
+	string str;
+	cin>>str;
+
+	ispalindrome(str);
+
 	return 0;
 }
