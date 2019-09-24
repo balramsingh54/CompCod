fe61d896606fad5e1a474bbbe132bb5c030e7186
Balram <singhbalram054@gmail.com>
2019-09-24 17:50:40 +0530

    remove duplicate

---
+++ c++/string/duplicate.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void remove_duplicate(string str)
+{
+	char arr[str.length()];
+	arr[0]=str[0];
+
+	for (int i = 1; i < str.length(); i++)
+	{
+		for (int j = 1; j < str.length(); j++)
+		{
+			if (str[i]!=arr[j])
+			{
+				arr[j]=str[i];
+			}
+			else
+				break;
+		}
+	}
+	for (int i = 0; i < str.length(); i++)
+	{
+		cout<<arr[i];
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	string str;
+	getline(cin, str);
+
+	remove_duplicate(str);
+	return 0;
+}
