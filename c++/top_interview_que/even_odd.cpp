f4aae1b066d400e8c91b3007453cbf10dd2f398d
Balram <singhbalram054@gmail.com>
2019-10-01 16:57:56 +0530

    google question

---
+++ c++/top_interview_que/even_odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int no;
+	cin>>no;
+	if (no%2==0)
+	{
+		cout<<"this is even";
+	}
+	else
+	{
+		cout<<"this is odd";
+	}
+
+	return 0;
+}
