9356bab72c68fd555751d93a3f5c7c366a3d8788
Balram <singhbalram054@gmail.com>
2019-10-01 18:03:34 +0530

    leap year

--- c++/top_interview_que/leap_year.cpp
+++ c++/top_interview_que/leap_year.cpp
@@ -5,7 +5,17 @@ int main(int argc, char const *argv[])
 {
 	int year;
 	cin>>year;
-	if (year%4==0)
+	if (year%4==0 and year%100!=0)
+	{
+		cout<<"this is leap year";
+	}
+
+	else if (year%100==0 and year%400==0)
+	{
+		cout<<"this is leap year";
+	}
+
+	else if (year%400==0)
 	{
 		cout<<"this is leap year";
 	}
