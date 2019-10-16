4d7ebb3b744f24a6c0ddc2c29370553a57ad321d
Balram <singhbalram054@gmail.com>
2019-10-16 22:54:02 +0530

    has map

---
+++ DataStru/hash_map/hasmap.h
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template< typename T>
+
+class node
+{
+public:
+	string key;
+	T value;
+	node<T>* next;
+
+	node(string k, T v)
+	{
+		key= k;
+		value=v;
+		next=NULL;
+	}
+
+};
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
