adbbaaedeb71be8eed31c74d7bcb7c71f6f614b4
Balram <singhbalram054@gmail.com>
2019-10-18 21:47:49 +0530

    has table

--- DataStru/hash_map/hasmap.h
+++ DataStru/hash_map/hasmap.h
@@ -23,28 +23,38 @@ template<typename T>
 
 class hash_table
 {
-	int cs;
-	int ts;
+	int current_Size=0;
+	int max_Size=0;
 	node<T>** bucket;
 
 	int has_func(string key)
 	{
+		int idx=0;
 		int L= key.length();
-		int current_Size=0;
-		int max_Size=0;
-		int m_factor=0;
+		int m_factor=1;
 
 		for (int i = 0; i < L; ++i)
 		{
-			int idx= key[L-i-1]*m_factor;
+			idx= idx+key[L-i-1]*m_factor;
 			m_factor = m_factor*37;
 			m_factor = m_factor%max_Size;
 			idx = idx%max_Size; 
 		}
+		return idx;
+	}
+
+	hash_table(int default_size=7)
+	{
+		int max_Size=default_size;
+		bucket= new node<T>* [default_size];
+
+		for (int i = 0; i < max_Size; i++)
+		 {
+		 	bucket[i]=NULL;
+		 } 
 	}
 
 };
-
 
 int main(int argc, char const *argv[])
 {
