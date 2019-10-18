aeefc3d853413bd73f4aba5344e360c6f2f34d8d
Balram <singhbalram054@gmail.com>
2019-10-18 21:19:52 +0530

    has index

--- DataStru/hash_map/hasmap.h
+++ DataStru/hash_map/hasmap.h
@@ -19,12 +19,31 @@ public:
 
 };
 
+template<typename T>
+
 class hash_table
 {
-	
-	
-};
+	int cs;
+	int ts;
+	node<T>** bucket;
 
+	int has_func(string key)
+	{
+		int L= key.length();
+		int current_Size=0;
+		int max_Size=0;
+		int m_factor=0;
+
+		for (int i = 0; i < L; ++i)
+		{
+			int idx= key[L-i-1]*m_factor;
+			m_factor = m_factor*37;
+			m_factor = m_factor%max_Size;
+			idx = idx%max_Size; 
+		}
+	}
+
+};
 
 
 int main(int argc, char const *argv[])
