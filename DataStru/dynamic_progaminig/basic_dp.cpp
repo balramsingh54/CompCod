03dc7b21a4ea2fbab583f6ebb3701a9632adff24
Balram <singhbalram054@gmail.com>
2019-10-20 10:47:32 +0530

    dynamic progamming

--- DataStru/dynamic_progaminig/basic_dp.cpp
+++ DataStru/dynamic_progaminig/basic_dp.cpp
@@ -15,11 +15,25 @@ int fib(int n)
 	return ans;
 }
 
+
+int fib_DP(int n)
+{
+	int MEMO[n+1];
+	MEMO[0]=0;
+	MEMO[1]=1;
+	for (int i = 2; i <= n; i++)
+	{
+		MEMO[i] = MEMO[i-1]+ MEMO[i-2];
+	}
+	return MEMO[n];
+}
+
 int main(int argc, char const *argv[])
 {
 	int n;
 	cin>>n;
-	cout<<fib(n);
+	//cout<<fib(n);
 
+	cout<<fib_DP(n);
 	return 0;
 }
